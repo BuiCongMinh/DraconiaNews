adc6b8d766b51c1e071bd759fab27866ae59d8ad
Test_Terminal_git <minhvnvnvn1998@gmail.com>
2024-08-19 23:15:17 +0700

    update doom

--- src/App.js
+++ src/App.js
@@ -19,21 +19,21 @@ const swiper = new Swiper('.mySwiper', {
         delay: 3000,
         pauseOnMouseEnter: true
     },
-    
+
     speed: 1000,
 
 });
 
 
 // Change tabs 
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+const doom = document.querySelector.bind(document);
+const doomdoom = document.querySelectorAll.bind(document);
 
-const tabItems = $$('.tab-item')
-const contents = $$('.content')
+const tabItems = doomdoom('.tab-item')
+const contents = doomdoom('.content')
 
-const tabActive = $('.tab-item.active')
-const line = $('.tabs .line')
+const tabActive = doom('.tab-item.active')
+const line = doom('.tabs .line')
 
 line.style.width = tabActive.offsetWidth + "px"
 line.style.left = tabActive.offsetLeft + "px"
@@ -41,8 +41,8 @@ line.style.left = tabActive.offsetLeft + "px"
 tabItems.forEach((tab, index) => {
     const content = contents[index]
     tab.onclick = function () {
-        $('.tab-item.active').classList.remove('active')
-        $('.content.active').classList.remove('active')
+        doom('.tab-item.active').classList.remove('active')
+        doom('.content.active').classList.remove('active')
 
         line.style.width = this.offsetWidth + "px"
         line.style.left = this.offsetLeft + "px"
